7c238c4b6230cfd0c71b629d5686d3bb09c018ec
Prajwal Tarpe <prajwaltarpe8@gmail.com>
2024-12-19 13:45:34 +0530

    Initial commit for URL Shortener project

---
+++ routes/url.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const {handleGenerateNewShortURl,handleGetAnalytics} = require("../controllers/url");
+const router = express.Router();
+
+router.post("/", handleGenerateNewShortURl)
+
+router.get('/analytics/:shortId',handleGetAnalytics)
+
+module.exports = router;
